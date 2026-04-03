6860b7be6dca328c3c329b289ac373aaca65999f
Lev2307 <Unfortunately1366@gmail.com>
2026-04-03 16:48:28 +0300

    подправил пути до файлов

--- student_assistant/bot/faq_and_logs.py
+++ student_assistant/bot/faq_and_logs.py
@@ -4,13 +4,13 @@ from aiofile import async_open
 
 async def save_requests(data: dict):
     # перезаписывает в файл
-    async with async_open('student_assistant/bot/requests.jsonl', encoding='utf-8', mode='a') as f:
+    async with async_open('bot/requests.jsonl', encoding='utf-8', mode='a') as f:
         payload = json.dumps(data, ensure_ascii=False)
         await f.write(payload)
         await f.write("\n")
 
 async def get_possible_options() -> list[dict]:
-    async with async_open('student_assistant/bot/faq.json', encoding='utf-8', mode='r') as f:
+    async with async_open('bot/faq.json', encoding='utf-8', mode='r') as f:
         payload = await f.read()
         config_data = json.loads(payload)
         return config_data
--- student_assistant/bot/suggestions.py
+++ student_assistant/bot/suggestions.py
@@ -3,13 +3,13 @@ import json
 from aiofile import async_open
 
 async def save_suggestion(data: dict):
-    async with async_open('student_assistant/bot/suggestions.jsonl', encoding='utf-8', mode='a') as f:
+    async with async_open('bot/suggestions.jsonl', encoding='utf-8', mode='a') as f:
         payload = json.dumps(data, ensure_ascii=False)
         await f.write(payload)
         await f.write("\n")
 
 async def get_all_suggestions() -> list[dict]:
-    async with async_open('student_assistant/bot/suggestions.jsonl', encoding='utf-8', mode='r') as f:
+    async with async_open('bot/suggestions.jsonl', encoding='utf-8', mode='r') as f:
         payload = await f.read()
         data = [json.loads(line) for line in payload.splitlines()]
         return data
